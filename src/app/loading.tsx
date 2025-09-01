36d33bcfbe1c6cd5decb1dcce341286d0a9e7ceb
Fiona <fionat119@icloud.com>
2025-09-01 14:25:54 -0600

    updated not found and loading pages

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,13 +3,14 @@ import Image from "next/image";
 export default function Loading() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Image
-        src="/gifs/lights_out.gif"
-        alt="loading animation"
-        width={600}
-        height={600}
-        className="flex items-center justify-center"
-      />
+      <div className="relative h-1/2 w-1/2">
+        <Image
+          src="/gifs/not_found.gif"
+          alt="loading animation"
+          fill
+          className="object-contain"
+        />
+      </div>
     </div>
   );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,14 +3,15 @@ import Image from "next/image";
 export default function NotFound() {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-8">
-      <Image
-        src="/gifs/not_found.gif"
-        alt="loading animation"
-        width={400}
-        height={400}
-        className="flex items-center justify-center"
-      />
-      <span className="text-lg font-semibold text-red-500">
+      <div className="relative h-1/2 w-3/4">
+        <Image
+          src="/gifs/lights_out.gif"
+          alt="loading animation"
+          fill
+          className="object-contain"
+        />
+      </div>
+      <span className="text-lg font-semibold text-red-500 md:text-xl">
         404 - Page Not Found...
       </span>
     </div>
