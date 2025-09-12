32f0643429c0f9a512aba226ebbca5516754a800
Fiona <fionat119@icloud.com>
2025-09-12 11:32:34 -0600

    fixed carousel

--- src/components/landingPage/carousel.tsx
+++ src/components/landingPage/carousel.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useState } from "react";
 import CarouselCards from "./carouselCards";
 import Image from "next/image";
-import { motion } from "motion/react";
 
 const cards = [
   {
-    imgSr: "/landingPage/driver_trophy.png",
+    imgSrc: "/landingPage/driver_trophy.png",
     link: "/",
     feature: "Drivers Standings →",
     hoverText: "See who is leading the driver's championship!",
@@ -45,37 +44,61 @@ const cards = [
 ];
 
 export default function Carousel() {
-  const scrollRef = useRef<HTMLDivElement>(null);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const prevSlide = () => {
+  const prevIndex = currentIndex === 0 ? cards.length - 1 : currentIndex - 1;
+  const nextIndex = currentIndex === cards.length - 1 ? 0 : currentIndex + 1;
+  const prevCard = () => {
     setCurrentIndex((prev) => (prev === 0 ? cards.length - 1 : prev - 1));
   };
-  const nextSlide = () => {
+  const nextCard = () => {
     setCurrentIndex((prev) => (prev === cards.length - 1 ? 0 : prev + 1));
   };
 
   return (
-    <div className="mb-8 flex flex-row gap-10">
-      <Image
-        src="/svgs/arrow.svg"
-        alt="Left arrow"
-        width={30}
-        height={30}
-        className="transition ease-in-out hover:scale-110"
-      />
-      <CarouselCards
-        imgSrc="/landingPage/driver_trophy.png"
-        link="/"
-        feature="Drivers Standings →"
-        hoverText="See who is leading the driver's championship!"
-      />
-      <Image
-        src="/svgs/arrow.svg"
-        alt="Right arrow"
-        width={30}
-        height={30}
-        className="rotate-180 transition ease-in-out hover:scale-110"
-      />
+    <div className="mb-8 flex flex-col items-center justify-center gap-5">
+      <div className="flex flex-row gap-10">
+        <button onClick={prevCard} className="transition hover:scale-110">
+          <Image
+            src="/svgs/arrow.svg"
+            alt="Left arrow"
+            width={30}
+            height={30}
+            className="transition ease-in-out hover:scale-110"
+          />
+        </button>
+
+        <CarouselCards
+          imgSrc={cards[currentIndex].imgSrc}
+          link={cards[currentIndex].link}
+          feature={cards[currentIndex].feature}
+          hoverText={cards[currentIndex].hoverText}
+          prevImgSrc={cards[prevIndex].imgSrc}
+          prevFeature={cards[prevIndex].feature}
+          nextImgSrc={cards[nextIndex].imgSrc}
+          nextFeature={cards[nextIndex].feature}
+        />
+
+        <button onClick={nextCard} className="transition hover:scale-110">
+          <Image
+            src="/svgs/arrow.svg"
+            alt="Right arrow"
+            width={30}
+            height={30}
+            className="rotate-180 transition ease-in-out hover:scale-110"
+          />
+        </button>
+      </div>
+      <div className="flex gap-3">
+        {cards.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => setCurrentIndex(index)}
+            className={`h-2 w-2 rounded-full transition ${
+              index === currentIndex ? "scale-110 bg-ocean-blue" : "bg-gray-400"
+            }`}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/landingPage/carouselCards.tsx
+++ src/components/landingPage/carouselCards.tsx
@@ -6,6 +6,10 @@ interface CarouselCardsProps {
   feature: string;
   link: string;
   hoverText: string;
+  prevImgSrc: string;
+  prevFeature: string;
+  nextImgSrc: string;
+  nextFeature: string;
 }
 
 export default function CarouselCards({
@@ -13,26 +17,54 @@ export default function CarouselCards({
   feature,
   link,
   hoverText,
+  prevImgSrc,
+  prevFeature,
+  nextImgSrc,
+  nextFeature,
 }: CarouselCardsProps) {
   return (
-    <Link href={link}>
-      <div className="group relative h-52 w-[350px] overflow-hidden rounded-xl">
+    <div className="flex items-center">
+      <div className="group relative z-0 overflow-hidden rounded-xl opacity-70 md:-mr-12 md:h-44 md:w-[250px] lg:-mr-32 lg:h-52 lg:w-[350px]">
         <Image
-          src={imgSrc}
-          alt={`${feature} image`}
+          src={prevImgSrc}
+          alt={`${prevFeature} image`}
           fill
           className="object-cover transition-transform duration-300 group-hover:scale-105"
         />
         <span className="bg-moody-grey/40 absolute left-1/2 top-1 z-10 inline-block -translate-x-1/2 items-center justify-center whitespace-nowrap rounded-md px-2 pt-1 text-center text-white">
-          {feature}
+          {prevFeature}
         </span>
-
-        <div className="absolute inset-0 flex items-center justify-center bg-black/50 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-          <span className="px-2 text-center text-sm text-white">
-            {hoverText}
-          </span>
-        </div>
       </div>
-    </Link>
+      <Link href={link}>
+        <div className="group relative z-10 overflow-hidden rounded-xl md:h-52 md:w-[350px] lg:h-64 lg:w-[400px]">
+          <Image
+            src={imgSrc}
+            alt={`${feature} image`}
+            fill
+            className="object-cover transition-transform duration-300 group-hover:scale-105"
+          />
+          <span className="bg-moody-grey/40 absolute left-1/2 top-1 z-10 inline-block -translate-x-1/2 items-center justify-center whitespace-nowrap rounded-md px-2 pt-1 text-center text-white">
+            {feature}
+          </span>
+
+          <div className="absolute inset-0 flex items-center justify-center bg-black/50 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+            <span className="px-2 text-center text-sm text-white">
+              {hoverText}
+            </span>
+          </div>
+        </div>
+      </Link>
+      <div className="group relative z-0 -ml-12 overflow-hidden rounded-xl opacity-70 md:h-44 md:w-[250px] lg:-ml-32 lg:h-52 lg:w-[350px]">
+        <Image
+          src={nextImgSrc}
+          alt={`${nextFeature} image`}
+          fill
+          className="object-cover transition-transform duration-300 group-hover:scale-105"
+        />
+        <span className="bg-moody-grey/40 absolute left-1/2 top-1 z-10 inline-block -translate-x-1/2 items-center justify-center whitespace-nowrap rounded-md px-2 pt-1 text-center text-white">
+          {nextFeature}
+        </span>
+      </div>
+    </div>
   );
 }
